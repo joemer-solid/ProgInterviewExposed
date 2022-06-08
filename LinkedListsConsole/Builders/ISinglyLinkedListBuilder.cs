08d3327bb31bcbab668711dc819d13a796951051
joemer-solid <joe_mer@hotmail.com>
2022-06-08 17:51:44 -0400

    removed unused usings

--- LinkedListsConsole/Builders/ISinglyLinkedListBuilder.cs
+++ LinkedListsConsole/Builders/ISinglyLinkedListBuilder.cs
@@ -1,12 +1,6 @@
-﻿using LinkedListsConsole.Elements;
-using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace LinkedListsConsole.Builders
+﻿namespace LinkedListsConsole.Builders
 {
     public interface ISinglyLinkedListBuilder<T, P> : ILinkedListBuilder<T, P>// where T : ListElement<T>
        // where P : IEnumerable<P>
     { }
-    
 }
